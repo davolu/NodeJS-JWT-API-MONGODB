eca60362a46198ca449298b5fc0cb275e22734e0
davolu <oluyaled@gmail.com>
2020-01-19 01:36:26 +0100

    moongose connection implemented

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
-
+    
+//connect to mongo db
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true, useCreateIndex:true}
+  )
+  .then(() => console.log('DB Connected'))
+   
+  mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+  });
+  
+//route
 app.get('/', (req, res)=>{
     res.send('Hello from nodeJs');
 });
 
+//port
 const port = process.env.PORT || 8000;
 
+//start app
 app.listen(port, ()=>{
    console.log(`Server is running on port ${port}`);
 });
